779c34b2b1cb599e547135ac6e5cc6948c63a313
WizVasu <vasumittal8504@gmail.com>
2023-06-09 16:59:11 +0530

    Commited

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
-// const pp = require('popups');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutpageContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactpageContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const homestuff = "The saved responses are as follows";
+const aboutstuff = "There is usage of nodeJS, DJango, CSS, HTML";
+const contactstuff = "Vasu Mittal \n BITS Pilani";
 
 const app = express();
 
@@ -32,7 +30,7 @@ app.get("/", function (req, res) {
   Post.find({}, function (err, posts) { 
     if(!err){
       res.render("home", {
-        startContent: homeStartingContent,
+        startContent: homestuff,
         posts: posts
       });
     }
@@ -40,11 +38,11 @@ app.get("/", function (req, res) {
  })
 
 app.get("/about", function (req, res) { 
-  res.render("about", {aboutContent : aboutpageContent});
+  res.render("about", {aboutContent : aboutstuff});
  })
  
  app.get("/contact", function (req, res) { 
-  res.render("contact", {contactContent : contactpageContent});
+  res.render("contact", {contactContent : contactstuff});
  })
  
  app.get("/delete", function(req, res){
@@ -75,7 +73,7 @@ app.get("/about", function (req, res) {
 
 
   app.post("/delete", function(req,res){
-    // const Deltitle = req.body.postToDelete;
+    
     const DelId = req.body;
     const id = Object.keys(DelId);
     id.forEach((i) =>   Post.findOneAndDelete({_id: id},function(err, mes){
@@ -94,6 +92,8 @@ app.get("/about", function (req, res) {
   })
 
 
+
+
 app.get("/posts/:postId", function (req, res) { 
   const reqPostId = req.params.postId;
 
@@ -106,13 +106,7 @@ app.get("/posts/:postId", function (req, res) {
     }
    })
 
-  // posts.forEach(function (post) { 
-  //   const storedTitle = _.lowerCase(post.title);
-  //   if (reqTitle === storedTitle) {
-  //     res.render("post", {requestTitle: post.title, requestContent: post.content});
-  //   }
-  //  });
-   
+
   });
 
 app.listen(3000, function() {
